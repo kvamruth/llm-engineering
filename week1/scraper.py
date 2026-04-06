7aceca90243813ec2643885ec650c7ca01510e58
kvamruth <kvamruth017@gmail.com>
2026-04-06 16:45:18 +0000

    web scraping implemented

---
+++ week1/scraper.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+}
+
+def fetch_Website_contents(url):
+    response = requests.get(url,headers=headers)
+    print(response)
+    soup = BeautifulSoup(response.content,"html.parser")
+    title = soup.title.string if soup.title else "No title found"
+    for irr in soup.body(["script","style","img","input"]):
+        irr.decompose()
+    text = soup.body.get_text(separator="\n",strip=True)
+    return title + "\n\n" + text
